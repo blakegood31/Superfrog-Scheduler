4a174432b72d2ae62681d24fe472c2775c8970d5
marybethwalsh <mary.beth.walsh@tcu.edu>
2024-04-17 13:02:32 -0500

    converted request_page_1.html to RequestPage1.vue

--- HTML/src/main.js
+++ HTML/src/main.js
@@ -4,4 +4,12 @@ import './style.css'
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    //register components
+    .component('RequestPage1', RequestPage1)
+    .component('RequestPage2', RequestPage1)
+    .component('RequestPage3', RequestPage1)
+    .component('RequestPage4', RequestPage1)
+    .component('RequestPage5', RequestPage1)
+    .mount('#app')
