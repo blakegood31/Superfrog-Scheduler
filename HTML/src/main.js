733abf0da740783ca7bed758219feb2b873593a0
marybethwalsh <mary.beth.walsh@tcu.edu>
2024-04-17 10:18:43 -0500

    calendar

--- HTML/src/main.js
+++ HTML/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import './style.css'
-import 'element-plus/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
