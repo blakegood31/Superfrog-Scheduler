4f623f76514825af8184f46021c69cb141f07c1a
marybethwalsh <mary.beth.walsh@tcu.edu>
2024-04-24 10:35:18 -0500

    refresh

--- HTML/src/router/index.js
+++ HTML/src/router/index.js
@@ -1,19 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
+//import route components
 import CustomerLanding from '../views/CustomerLanding.vue';
 import RequestPage1 from '../views/RequestPage1.vue';
 import RequestPage2 from '../views/RequestPage2.vue';
 import RequestPage3 from '../views/RequestPage3.vue';
+import RequestPage4 from '../views/RequestPage4.vue';
+import RequestPage5 from '../views/RequestPage5.vue';
+
+//create the router instance
+const router = createRouter({
+    //provide the history implementation to use
+    history: createWebHistory(),
+});
 
 const routes = [
     { path: '/custland', component: CustomerLanding },
     { path: '/page1', component: RequestPage1 },
     { path: '/page2', component: RequestPage2 },
-    { path: '/page3', component: RequestPage3 }
+    { path: '/page3', component: RequestPage3 },
+    { path: '/page4', component: RequestPage4 },
+    { path: '/page5', component: RequestPage5 }
 ];
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
 
 export default router;
--- HTML/src/views/RequestPage3.vue
+++ HTML/src/views/RequestPage3.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!--TO DO: delete request_page_1.html-->
+
         <!-- progress bar -->
             <Progressbar />
 
